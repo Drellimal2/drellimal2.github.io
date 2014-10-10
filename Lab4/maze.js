b28ba82280b67b437c76398d8488d400da45a5cf
Dane Miller <dmiller2310@hotmail.com>
2014-10-10 15:57:39 -0500

    minor fixes

--- Lab4/maze.js
+++ Lab4/maze.js
@@ -4,6 +4,7 @@ Date: 03/10/2014*/
 
 "use strict";
 var canwin = true;
+var win = false;
 
 window.onload = function(){
     $("boundary1").onmouseover = setSingleLose;
@@ -30,18 +31,23 @@ var setLose = function(){
 };
 
 var endgame = function(){
-    if (canwin){
-        //alert("You won! Good Job");
-        $$("h2#status")[0].innerText ="You Win";
-    } else {
-        //alert("You Lost :'( Try again.");
-        $$("h2#status")[0].innerText ="You Lose";
-
+    if (!win){
+        if (canwin){
+            //alert("You won! Good Job");
+            $$("h2#status")[0].innerText ="You Win";
+            win = true;
+        } else {
+            //alert("You Lost :'( Try again.");
+            win = false;
+            $$("h2#status")[0].innerText ="You Lose";
+    
+        }
     }
 };
 
 var resetgame = function(){
     canwin = true;
+    win=false;
     var boundaries = $$('div.boundary');
     for(var counter = 0; counter< boundaries.length; counter++){
         boundaries[counter].removeClassName("youlose");
@@ -51,6 +57,8 @@ var resetgame = function(){
 };
 
 var outercheat = function(){
-    $$("h2#status")[0].innerText ="Please stay within the maze.\nClick S to reset";
-    canwin = false;
+    if(!win && canwin){
+        $$("h2#status")[0].innerText ="Please stay within the maze.\nClick S to reset";
+        canwin = false;
+    }
 }
